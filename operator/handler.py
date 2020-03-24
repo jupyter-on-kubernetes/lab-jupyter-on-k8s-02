60a9f3c65e8b105d6789be8cd2f0fe86b6c36925
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-03-24 16:30:39 +1100

    Add skeleton for start of operator implementation.

---
+++ operator/handler.py
@@ -0,0 +1,17 @@
+import os
+import random
+import string
+
+import kopf
+import kubernetes
+import kubernetes.client
+
+
+@kopf.on.create("jupyter-on-kubernetes.test", "v1alpha1", "jupyternotebooks")
+def workshop_request_create(name, uid, namespace, spec, logger, **_):
+    custom_objects_api = kubernetes.client.CustomObjectsApi()
+
+    return {
+      "url": "http://example.com",
+      "password": "password"
+    }

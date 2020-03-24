dbc1913a8c62ed05e7582fcf3c7e24e23b84f6a5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-03-24 20:29:07 +1100

    Missing option.

--- workshop/setup.d/setup-operator.sh
+++ workshop/setup.d/setup-operator.sh
@@ -4,6 +4,6 @@ if [ x"$KUBERNETES_SERVICE_HOST" != x"" ]; then
     if [ ! -d operator/bin/activate ]; then
         python3 -m venv operator
         source operator/bin/activate
-        pip install operator/requirements.txt
+        pip install -r operator/requirements.txt
     fi
 fi

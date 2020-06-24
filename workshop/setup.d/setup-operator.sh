5c98e5adfa01acb4a59c0a8f5972dbdb08e0c7a8
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-06-24 15:08:27 +1000

    Change local of virtual environment for the operator.

--- workshop/setup.d/setup-operator.sh
+++ workshop/setup.d/setup-operator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ x"$KUBERNETES_SERVICE_HOST" != x"" ]; then
-    if [ ! -d operator/bin/activate ]; then
-        python3 -m venv operator
-        source operator/bin/activate
+    if [ ! -d operator-venv/bin/activate ]; then
+        python3 -m venv operator-venv
+        source operator-venv/bin/activate
         pip install -r operator/requirements.txt
     fi
 fi
